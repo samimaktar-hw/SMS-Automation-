563ce5327322e15cf5caa20f616bd726b170176b
SAMIM AKTAR( Hexca web) <aktarsekhsamim25@gmail.com>
2026-09-20 11:16:06 +0530

    Update author name in HexcaWeb.sh

--- HexcaWeb.sh
+++ HexcaWeb.sh
@@ -29,7 +29,7 @@ banner() {
     if ! [ -x "$(command -v figlet)" ]; then
         echo 'Introducing Hexca web'
     else
-        figlet Hexca web 
+        figlet Hexcaweb 
     fi
     if ! [ -x "$(command -v toilet)" ]; then
         echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSamim \e[0m"
@@ -98,7 +98,7 @@ else
     echo .
     echo .
     install_deps
-    echo This Script Was Made By SpeedX > .update
+    echo This Script Was Made By Samim > .update
     echo 'Requirements Installed....'
     pause
 fi
